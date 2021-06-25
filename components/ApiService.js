f0428bf54da80d03eb395c9aec57ad69d03bcbab
chica25 <pfco520@gmail.com>
2021-06-25 01:16:54 -0400

    Edit the fetch method on Api.js

--- components/ApiService.js
+++ components/ApiService.js
@@ -1,14 +1,23 @@
-class ApiService {
-    constructor(baseUrl) {
-        this.baseUrl = baseUrl;
-    }
+// class ApiService {
+//     constructor(baseUrl) {
+//         this.baseUrl = baseUrl;
+//     }
 
 
+// fetchIceCreams = () => {
+//     const url = new URL(this.baseUrl + "/ice_creams")
+//     fetch(url).then(resp => resp.json())
+//     .then(data => IceCream.createIceCream(data));
 
-fetchIceCreams = () => {
-    const url = new URL(this.baseUrl + "/ice_creams")
-    fetch(url).then(resp => resp.json())
-    .then(data => IceCream.createIceCream(data));
+//     }
 
+// }
 
-}
+fetch('http://localhost:3000/ice_cream').then((response) => {
+    console.log('resolved', response);
+       return response.json();
+    }).then(data => {
+        console.log(data)
+    }).catch((err) => {
+        console.log('rejected', err);
+});
--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
-const api = new ApiService('http://localhost:3000/ice_cream')
+// const api = new ApiService('http://localhost:3000/ice_cream')
 
-document.addEventListener("DOMContentLoaded", () => {
-    api.fetchIceCreams();
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//     // api.fetchIceCreams();
+// })
